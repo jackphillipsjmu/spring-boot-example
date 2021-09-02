13b5ce6dc09b88e96b97e8c2d54e69f81799df13
Jack Phillips <6548883+jackphillipsjmu@users.noreply.github.com>
2021-09-01 20:11:51 -0400

    Delete build directory
    Again pre-gitignore knowledge like a dumby round 4

--- build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
--- On startup of application insert records into in memory Todo table
--- Notice you may either specify true/false or 'true'/'false' for booleans
-INSERT INTO todo (id, is_complete, message) VALUES
-  (1, false, 'Incomplete todo (Autgenerated Example)'),
-  (2, 'true', 'Complete todo (Autgenerated Example)');
